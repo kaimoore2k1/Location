676649a0bd85a57ccbb6aae455bd42aa09401d38
Kai Moore <haohao12012001@gmail.com>
2022-09-27 00:08:20 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,97 @@
+import data from "./data.json" assert { type: "json" };
+
+require([
+  "esri/config",
+  "esri/Map",
+  "esri/views/MapView",
+
+  "esri/Graphic",
+  "esri/layers/GraphicsLayer",
+], function (esriConfig, Map, MapView, Graphic, GraphicsLayer) {
+  esriConfig.apiKey =
+    "AAPKd54c7848d01f4691855b449d9afd1b31OjW4q0xS3HCLHUGZ8Io-FG8HM4UEA9QCsJkHQ584qtsYwlfKLZzKc7mXrPLU30Lt";
+  const map = new Map({
+    basemap: "arcgis-topographic", // Basemap layer service
+  });
+
+  const view = new MapView({
+    map: map,
+    center: [105.1366886, 9.7924769], // Longitude, latitude
+    zoom: 13, // Zoom level
+    container: "viewDiv", // Div element
+  });
+
+  const popupTemplate = {
+    title: "{Name}",
+    content: "{Description}",
+  };
+
+  const graphicsLayer = new GraphicsLayer();
+  map.add(graphicsLayer);
+
+  data.map((item) => {
+    switch (item.geometry.type) {
+      case "point":
+        const simpleMarkerSymbol = {
+          type: "picture-marker",
+          url: "./assets/icons/location_on_black_24dp.svg",
+          height: "27px",
+          width: "27px",
+        };
+        const pointGraphic = new Graphic({
+          geometry: item.geometry,
+          symbol: simpleMarkerSymbol,
+
+          attributes: {
+            Name: item.name,
+            Description: item.description,
+          },
+          popupTemplate,
+        });
+        graphicsLayer.add(pointGraphic);
+        break;
+      case "polyline":
+        const simpleLineSymbol = {
+          type: "simple-line",
+          color: [226, 119, 40], // Orange
+          width: 2,
+        };
+        const polylineGraphic = new Graphic({
+          geometry: item.geometry,
+          symbol: simpleLineSymbol,
+
+          attributes: {
+            Name: item.name,
+            Description: item.description,
+          },
+          popupTemplate,
+        });
+        graphicsLayer.add(polylineGraphic);
+        break;
+      case "polygon":
+        const simpleFillSymbol = {
+          type: "simple-fill",
+          color: item.color, // Orange, opacity 80%
+          outline: {
+            color: [255, 255, 255],
+            width: 1,
+          },
+        };
+
+        const polygonGraphic = new Graphic({
+          geometry: item.geometry,
+          symbol: simpleFillSymbol,
+
+          attributes: {
+            Name: item.name,
+            Description: item.description,
+          },
+          popupTemplate,
+        });
+        graphicsLayer.add(polygonGraphic);
+        break;
+      default:
+        break;
+    }
+  });
+});
---
+++ readme.md
@@ -0,0 +1,52 @@
+# Remember to exchange the API_KEY by your own API_KEY on line 12 in **index.js** before run
+
+# The way to get API_KEY
+    - An API key is required to access [ArcGIS services](https://developers.arcgis.com/documentation/mapping-apis-and-services/services/).
+        1. Go to your [developer dashboard](https://developers.arcgis.com/dashboard/) to get an API key.
+        2. Copy the key as it will be used in the next step.
+
+# The way to get points list in map with **getCoordinatesList.html**
+    1. Install **Live Server** package
+    2. Open **getCoordinatesList.html** with **Live Server**
+    3. Press F12 and open *Console Tab*
+    4. Click everywhere in map to get a point, watch results in *Console Tab*
+    5. Press "z" to delete the last point
+    6. Press "Enter" to convert data to JSON, data will be displayed in **Console Tab** and deleted after press "Enter"
+    8. Copy the converted data and paste it into **data.json**
+
+# Some notes about **data.json**
+    1. The structure of the data:
+        - Point: 
+            +   
+                {
+                    "name": string,
+                    "description": string,
+                    "geometry": {
+                        "type": "point",
+                        "longitude": number,
+                        "latitude": number
+                }
+        - Polyline:
+            {
+                "name": string,
+                "description": string,
+                "geometry": {
+                    "type": "polyline",
+                    "paths": [
+                       [longitude: number, latitude:number],
+                    ]
+                }
+            }
+        - Polygon:
+            {
+                "name": string,
+                "description": string,
+                "color"?: [red: number, green: number, blue: number, opacity: number,]
+                "geometry": {
+                    "type": "polygon",
+                    "rings": [
+                        [longitude: number, latitude:number],
+                    ]
+                }
+            }
+    2. Key "name", "description", "geometry" are always available, key "color" is only available for **Polygon**
